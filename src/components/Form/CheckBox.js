4d95084c08f9e5564506f64bb49c732adab14f35
webmaxxSELF <webmxxself@163.com>
2017-02-12 16:23:55 +0800

    form-react

--- src/components/Form/CheckBox.js
+++ src/components/Form/CheckBox.js
@@ -3,9 +3,9 @@ class CheckBox extends Component{
     render(){
         return(
             <div>
-                A <input type="checkbox" name='choose' value='A' onChange={this.props.clicking}/>
-                B <input type="checkbox" name='choose' value='B' onChange={this.props.clicking}/>
-                C <input type="checkbox" name='choose' value='C' onChange={this.props.clicking}/>
+                A <input type="checkbox" name='choose' value='A' onChange={this.props.handleCheck}/>
+                B <input type="checkbox" name='choose' value='B' onChange={this.props.handleCheck}/>
+                C <input type="checkbox" name='choose' value='C' onChange={this.props.handleCheck}/>
             </div>
         )
     }
--- src/components/Form/RadioButtons.js
+++ src/components/Form/RadioButtons.js
@@ -1,14 +1,11 @@
 import React ,{Component} from 'react';
 class RadioButtons extends Component{
-    saying(){
-        console.log('i am a radio')
-    }
     render(){
         return (
             <div>
-                A <input onChange={this.props.saying} name="ARadio" type="radio" value="A"/>
-                B <input onChange={this.props.saying} name="ARadio" type="radio" defaultChecked value="B"/>
-                C <input onChange={this.props.saying} name="ARadio" type="radio" value="C"/>
+                A <input onChange={this.props.handleRadio} name="ARadio" type="radio" value="A"/>
+                B <input onChange={this.props.handleRadio} name="ARadio" type="radio" defaultChecked value="B"/>
+                C <input onChange={this.props.handleRadio} name="ARadio" type="radio" value="C"/>
             </div>
         )
     }
